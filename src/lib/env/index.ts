c1572a01685863bad49056d5619a0b55bf94baa3
jon890 <jon89071@gmail.com>
2025-11-17 18:42:09 +0900

    refactor: 사용하지 않는 환경 유틸리티 함수 export 제거
    - isDev, isProduction, isTest, isPreview, isLocal, isServer, isClient, getEnvInfo
    - 실제 사용처가 없어서 index.ts에서 export 제거
    - env.ts 파일 자체는 유지 (향후 필요시 사용 가능)

--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -7,18 +7,6 @@
 // 클라이언트 환경변수만 export (클라이언트 번들에 포함되어도 안전)
 export { clientEnv, type ClientEnv } from "./client.env";
 
-// 환경 유틸리티 함수들 (클라이언트에서도 사용 가능)
-export {
-  isDev,
-  isProduction,
-  isTest,
-  isPreview,
-  isLocal,
-  isServer,
-  isClient,
-  getEnvInfo,
-} from "../server/config/env";
-
 // 서버 환경변수는 직접 import하세요:
 // import { serverEnv } from '@/lib/env/server.env'
 export type { ServerEnv } from "./server.env";
