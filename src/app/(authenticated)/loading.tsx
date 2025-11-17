f2ead2e3bd54cc94d834f8bd2290e97a2445fcee
jon890 <jon89071@gmail.com>
2025-11-17 16:43:28 +0900

    🩹 Chore: LoadingUI 정리

--- src/app/(authenticated)/loading.tsx
+++ src/app/(authenticated)/loading.tsx
@@ -1,9 +1,9 @@
 import { PageLoadingSpinner } from "@/components/common/PageLoadingSpinner";
 
 /**
- * Settings 페이지 로딩 UI
+ * 페이지 로딩 UI
  * 페이지 이동 시 서버 컴포넌트 로드 중 자동으로 표시됨
  */
-export default function SettingsLoading() {
+export default function PageLoadingContainer() {
   return <PageLoadingSpinner />;
 }
